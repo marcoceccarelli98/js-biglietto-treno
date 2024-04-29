1d5da958422aef134fc3c5905346c38aae025fbc
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-04-29 15:43:18 +0200

    [MILESTONE 1]

--- script/main.js
+++ script/main.js
@@ -1,13 +1,6 @@
 'use strict';
 
 // INPUTS
-// - definizione costante per input numero di km //kmToTravel
-const kmToTravel = Number(prompt('Inserisci i km da percorrere'));
-console.log(kmToTravel);
-// - definizione costante per input età passeggero //userAge
-const userAge = Number(prompt('Inserisci l\'età del passeggero'));
-console.log(userAge);
-// PROGRAM
 // - definizione costante di prezzo al km (0.21 € al km) //priceKm
 const priceKm = 0.21;
 // - definizione costante sconto minorenni (20%) //minorSale 
@@ -15,27 +8,54 @@ const saleMinor = 20;
 // - definizione costante sconto minorenni (20%) //minorSale 
 const saleOver65 = 40;
 
-// - definizione variabile calcolo del prezzo totale (km * priceKm) //totalPrice 
-let totalPrice = kmToTravel * priceKm;
-console.log(totalPrice);
+let userAge = 0;
+let kmToTravel = 0;
 
-// - calcolo eventuali sconti in base all'età
-//     - 20% per i minorenni
-//     - 40% per gli over 65
-if (userAge < 18) {
-    // totalPrice = (totalPrice / 100) * (100 - saleMinor);
-    totalPrice = totalPrice - (totalPrice * (saleMinor / 100));
+const buttonGenerate = document.getElementById('generate');
+
+buttonGenerate.addEventListener('click', function () {
+    const elementAge = document.getElementById('age');
+    const elementKm = document.getElementById('km');
+
+    // - definizione costante per input età passeggero //userAge
+    userAge = Number(elementAge.value);
+
+    // - definizione costante per input numero di km //kmToTravel
+    kmToTravel = Number(elementKm.value);
+
+    console.log(userAge);
+    console.log(kmToTravel);
+
+    // - definizione variabile calcolo del prezzo totale (km * priceKm) //totalPrice 
+    let totalPrice = kmToTravel * priceKm;
+    console.log(totalPrice);
+
+    // - calcolo eventuali sconti in base all'età
+    //     - 20% per i minorenni
+    //     - 40% per gli over 65
+    if (userAge < 18) {
+        // totalPrice = (totalPrice / 100) * (100 - saleMinor);
+        totalPrice = totalPrice - (totalPrice * (saleMinor / 100));
+    }
+    else if (userAge > 65) {
+        totalPrice = (totalPrice / 100) * (100 - saleOver65);
+    }
+    console.log(totalPrice);
+    console.log(Math.round(totalPrice * 100) / 100);
+
 }
-else if (userAge > 65) {
-    totalPrice = (totalPrice / 100) * (100 - saleOver65);
-}
-console.log(totalPrice);
+)
+
 
 // OUTPUT
-document.getElementById('distance').innerHTML = kmToTravel;
-document.getElementById('age').innerHTML = userAge;
-document.getElementById('pricekm').innerHTML = priceKm;
-document.getElementById('minorsale').innerHTML = saleMinor;
-document.getElementById('over65sale').innerHTML = saleOver65;
+// document.getElementById('distance').innerHTML = kmToTravel;
+// document.getElementById('age').innerHTML = userAge;
+// document.getElementById('pricekm').innerHTML = priceKm;
+// document.getElementById('minorsale').innerHTML = saleMinor;
+// document.getElementById('over65sale').innerHTML = saleOver65;
 // - visualizzo il prezzo del biglietto arrotondando a due cifre decimali
-document.getElementById('totalprice').innerHTML = Math.round(totalPrice * 100) / 100;
+// document.getElementById('totalprice').innerHTML = Math.round(totalPrice * 100) / 100;
+
+
+
+
